c3dd962798d0c4bb45d269983711cc5b63fa210d
Evgeniy Kanareikin <kenor@tut.by>
2024-09-08 15:43:20 +0300

    small fixes

--- src/test/java/ru/clevertec/StringUtilsTest.java
+++ src/test/java/ru/clevertec/StringUtilsTest.java
@@ -1,17 +1,30 @@
 package ru.clevertec;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringUtilsTest {
-    @Test
-    @DisplayName("IsPositiveNumber method")
-    void testIsPositiveNumber() {
-        assertTrue(StringUtils.isPositiveNumber("123"));
-        assertFalse(StringUtils.isPositiveNumber("-123"));
-        assertFalse(StringUtils.isPositiveNumber("abc"));
-        assertFalse(StringUtils.isPositiveNumber("0"));
+    @DisplayName("IsPositiveNumber method with positive numbers")
+    @ParameterizedTest(name = "with positive numbers")
+    @ValueSource(strings = {"123", "1", "77"})
+    void shouldReturnTrueIfNumberIsPositive(String str) {
+        assertTrue(StringUtils.isPositiveNumber(str));
+    }
+
+    @DisplayName("IsPositiveNumber method with negative numbers")
+    @ParameterizedTest(name = "with negative numbers")
+    @ValueSource(strings = {"-123", "0", "-77"})
+    void shouldReturnFalseIfNumberIsNegativeOrZero(String str) {
+        assertFalse(StringUtils.isPositiveNumber(str));
+    }
+
+    @DisplayName("IsPositiveNumber method with NumberFormatException")
+    @ParameterizedTest(name = "with NumberFormatException")
+    @ValueSource(strings = {"abc", "D", "true"})
+    void shouldReturnFalseIfCatchNumberFormatException(String str) {
+        assertFalse(StringUtils.isPositiveNumber(str));
     }
 }
